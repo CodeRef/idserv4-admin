4eb57729e2377222ad915040744db9e9586740be
Wut.W <wut.w@excelbangkok.com>
2019-03-04 15:24:04 +0700

    Initial git

---
+++ IdServe/src/IdServe.Admin/Configuration/Interfaces/IRootConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace IdServe.Admin.Configuration.Interfaces
+{
+    public interface IRootConfiguration
+    {
+        IAdminConfiguration AdminConfiguration { get; }
+    }
+}
---
+++ IdServe/src/IdServe.Admin/Configuration/RootConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using IdServe.Admin.Configuration.Interfaces;
+
+namespace IdServe.Admin.Configuration
+{
+    public class RootConfiguration : IRootConfiguration
+    {
+        public IAdminConfiguration AdminConfiguration { get; set; }
+
+        public RootConfiguration(IOptions<AdminConfiguration> adminConfiguration)
+        {
+            AdminConfiguration = adminConfiguration.Value;
+        }
+    }
+}
